cdb15ae8bd7f6fdbc2ea23d855e2881d2ab9df54
Jess Dever <jessicadever1@gmail.com>
2021-04-01 11:05:15 -0500

    Comments Model created

---
+++ TabloidMVC/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
